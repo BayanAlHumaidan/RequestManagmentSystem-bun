93764f9cada2fa62043d3d6c2ff1797c8514ab67
Bayan Omar <bayanomar@iMac-Bayan.local>
2024-04-24 15:29:22 +0300

    Server set up

--- functions.ts
+++ functions.ts
@@ -1,4 +1,4 @@
-import {Database} from "bun:sqlite"
+import { Database } from "bun:sqlite"
 import { readFileSync } from "node:fs";
 import type { PathOrFileDescriptor } from 'fs';
 
@@ -62,87 +62,125 @@ export function createDbSchema(db: Database) {
     }
 }
 
-export function readCSV(filePath: PathOrFileDescriptor) {
+export function numOfRecords(db: Database) {
+    const tables = ['Requests', 'NewLicenseRequests', 'AccountRequests', 'InspectionRequests', 'NewActivityRequests', 'StampLicenseRequests'];
 
-    const data = readFileSync(filePath, 'utf8');
-    const normalizedData = data.replace(/\r\n/g, '\n').trim();
-    const rows = normalizedData.split('\n');
-    rows.shift();
-
-    const processedData = rows.map(row => {
-        const parts = row.split(',');
-
-        let rawJsonData = parts.slice(3).join(',');
-
-        let formattedJsonData = rawJsonData.replace(/^"|"$/g, '').replace(/""/g, '"');
-
-        const obj = {
-            RequestID: parts[0],
-            RequestType: parts[1],
-            RequestStatus: parts[2],
-            RequestData: formattedJsonData
-        };
-
-        try {
-            obj.RequestData = JSON.parse(obj.RequestData);
-        } catch (error) {
-            console.error(`Error parsing JSON for RequestID ${obj.RequestID}:`, error);
-            obj.RequestData = "";
-        }
-        console.log('Processed Row:', obj);
-        return obj;
-    });
-
-    return processedData;
+    tables.forEach(tableName => {
+        const result = db.exec(`SELECT COUNT(*) FROM ${tableName}`)
+        console.log(`${tableName}: ${result}`)
+    })
 }
 
 export function processCSVData(db: Database, csvData) {
     csvData.forEach(row => {
+        try {
+            // Insert into Requests table
+            insertDataIntoRequestsTable(db, row.RequestID, row.RequestType, row.RequestStatus);
 
-        // Insert into Requests table
-        insertDataIntoRequestsTable(db, row.RequestID, row.RequestType, row.RequestStatus);
-
-        // Insert into type-specific table
-        insertDataIntoTypeSpecificTable(db, row.RequestID, row.RequestType, row.RequestData);
+            // Insert into type-specific table
+            insertDataIntoTypeSpecificTable(db, row.RequestID, row.RequestType, row.RequestData);
+        } catch (e) {
+            console.error(`Failed to process row with RequestID ${row.RequestID}: ${e.message}`);
+        }
     });
 }
+export function readCSV(filePath: PathOrFileDescriptor) {
+    try {
+        const data = readFileSync(filePath, 'utf8');
+        const normalizedData = data.replace(/\r\n/g, '\n').trim();
+        const rows = normalizedData.split('\n');
+        rows.shift();
 
+        const processedData = rows.map(processRow);
+        return processedData;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(`Failed to read or process CSV file: ${error.message}`);
+        } else {
+            console.error(`Failed to read or process CSV file: An unknown error occurred`);
+        }
 
-
-export function insertDataIntoRequestsTable(db: Database, requestId, requestType, requestStatus) {
-    db.exec(`INSERT INTO Requests (RequestID, RequestType, RequestStatus) VALUES (?, ?, ?)`, [requestId, requestType, requestStatus]);
-}
-export function insertDataIntoTypeSpecificTable(db: Database, requestId, requestType, requestData) {
-    const data = requestData;
-
-    switch (requestType) {
-        case "1":
-            db.exec(`INSERT INTO NewLicenseRequests (RequestID, CompanyName, LicenceType, IsOffice, OfficeName, OfficeServiceNumber, RequestDate, Activities) VALUES (?,?, ?, ?, ?, ?, ?, ?)`,
-                [requestId, data.CompanyName, data.LicenceType, data.IsOffice, data.OfficeName, data.OfficeServiceNumber, data.RequestDate, data.Activities]);
-            break;
-        case "2":
-            db.exec(`INSERT INTO AccountRequests (RequestID, CompanyName, RequesterName, ApplicantName, UserName, ContactEmail, Permissions) VALUES (?, ?, ?, ?, ?, ?, ?)`,
-                [requestId, data.CompanyName, data.RequesterName, data.ApplicantName, data.UserName, data.ContactEmail, JSON.stringify(data.Permissions)]);
-            break;
-        case "3":
-            db.exec(`INSERT INTO InspectionRequests (RequestID, CompanyName, InspectionDate, InspectionTime, InspectionType) VALUES (?, ?, ?, ?, ?)`,
-                [requestId, data.CompanyName, data.InspectionDate, data.InspectionTime, data.InspectionType]);
-            break;
-        case "4":
-            db.exec(`INSERT INTO NewActivityRequests (RequestID, CompanyName, LicenceID, Activities) VALUES (?, ?, ?, ?)`,
-                [requestId, data.CompanyName, data.LicenceID, JSON.stringify(data.Activities)]);
-            break;
-        case "5":
-            db.exec(`INSERT INTO StampLicenseRequests (RequestID, CompanyName, LicenceID, RequestDate) VALUES (?, ?, ?, ?)`,
-                [requestId, data.CompanyName, data.LicenceID, data.RequestDate]);
-            break;
-        default:
-            console.log("Unknown Request Type:", requestType);
-            break;
+        return []; // Return an empty array 
     }
 }
 
-export function printTableData(tableName: string, db: Database) {
+function processRow(row) {
+    const parts = row.split(',');
+    let rawJsonData = parts.slice(3).join(',');
+    let formattedJsonData = rawJsonData.replace(/^"|"$/g, '').replace(/""/g, '"');
+
+    const obj = {
+        RequestID: parseInt(parts[0], 10),
+        RequestType: parseInt(parts[1], 10),
+        RequestStatus: parseInt(parts[2], 10),
+        RequestData: formattedJsonData
+    };
+
+    try {
+        obj.RequestData = JSON.parse(obj.RequestData);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(`Error parsing JSON for RequestID ${obj.RequestID}: ${error.message}`);
+        } else {
+            console.error(`Error parsing JSON for RequestID An unknown error occurred`);
+        }
+
+        obj.RequestData = "";
+    }
+    return obj;
+}
+
+function insertDataIntoRequestsTable(db: Database, requestId, requestType, requestStatus) {
+    try {
+        db.exec(`INSERT INTO Requests (RequestID, RequestType, RequestStatus) VALUES (?, ?, ?)`, [requestId, requestType, requestStatus]);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(`Failed to insert into Requests: ${error.message}`);
+        } else {
+            console.error(`Failed to insert into Requests: An unknown error occurred`);
+        }
+    }
+}
+function insertDataIntoTypeSpecificTable(db: Database, requestId, requestType, requestData) {
+    const data = requestData;
+
+    try {
+        switch (requestType) {
+            case 1:
+                db.exec(`INSERT INTO NewLicenseRequests (RequestID, CompanyName, LicenceType, IsOffice, OfficeName, OfficeServiceNumber, RequestDate, Activities) VALUES (?,?, ?, ?, ?, ?, ?, ?)`,
+                    [requestId, data.CompanyName, data.LicenceType, data.IsOffice, data.OfficeName, data.OfficeServiceNumber, data.RequestDate, data.Activities]);
+                break;
+            case 2:
+                db.exec(`INSERT INTO AccountRequests (RequestID, CompanyName, RequesterName, ApplicantName, UserName, ContactEmail, Permissions) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+                    [requestId, data.CompanyName, data.RequesterName, data.ApplicantName, data.UserName, data.ContactEmail, JSON.stringify(data.Permissions)]);
+                break;
+            case 3:
+                db.exec(`INSERT INTO InspectionRequests (RequestID, CompanyName, InspectionDate, InspectionTime, InspectionType) VALUES (?, ?, ?, ?, ?)`,
+                    [requestId, data.CompanyName, data.InspectionDate, data.InspectionTime, data.InspectionType]);
+                break;
+            case 4:
+                db.exec(`INSERT INTO NewActivityRequests (RequestID, CompanyName, LicenceID, Activities) VALUES (?, ?, ?, ?)`,
+                    [requestId, data.CompanyName, data.LicenceID, JSON.stringify(data.Activities)]);
+                break;
+            case 5:
+                db.exec(`INSERT INTO StampLicenseRequests (RequestID, CompanyName, LicenceID, RequestDate) VALUES (?, ?, ?, ?)`,
+                    [requestId, data.CompanyName, data.LicenceID, data.RequestDate]);
+                break;
+            default:
+                console.log("Unknown Request Type:", requestType);
+                break;
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(`Failed to insert into Request specific table: ${error.message}`);
+        } else {
+            console.error(`Failed to insert into Request specific table: An unknown error occurred`);
+        }
+
+    }
+}
+
+function printTableData(tableName: string, db: Database) {
     const query = `SELECT * FROM ${tableName}`;
     const results = db.query(query).all();
 
--- index.ts
+++ index.ts
@@ -1,34 +1,75 @@
 import {Database} from "bun:sqlite"
 import { readFileSync } from "node:fs";
 import type { PathOrFileDescriptor } from 'fs';
-
-import {createDbSchema,readCSV, processCSVData, printAllTables } from "./functions"
+import { promises as fs } from "fs"; 
+import {createDbSchema,readCSV, processCSVData, printAllTables,numOfRecords } from "./functions"
 
 const db = new Database("db.sqlite");
 
 const SamplefilePath = "./Data/Sample.csv"
 
+const uploadDir = "./RequestsUploads"; 
 
-
-function main(filePath) {
-    createDbSchema(db);
-    try {
-        const csvData = readCSV( filePath);
-
-        processCSVData(db, csvData);
-
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
-
-    try {
-        printAllTables(db);
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
-
+// Function to save the uploaded file and return the file path
+async function saveUploadedFile(file: File) {
+    const filePath = `${uploadDir}/${file.name}`;
+    await Bun.write(filePath, file); 
+    return filePath;
 }
 
-main(SamplefilePath);
+function main(filePath: string) {
+    createDbSchema(db);
+
+    try {
+        const csvData = readCSV(filePath);
+        processCSVData(db, csvData);
+      console 
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+    try {
+      numOfRecords(db)
+        // printAllTables(db);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
+// main(SamplefilePath);
 
 
+const server = Bun.serve({
+    port: 4000,
+    async fetch(req) {
+      const url = new URL(req.url);
+
+      // return index.html for root path
+      if (url.pathname === "/")
+        return new Response(Bun.file("./resources/index.html"), {
+          headers: {
+            "Content-Type": "text/html",
+          },
+        });
+
+      if (url.pathname === '/requests-upload' && req.method === "POST") {
+        const formdata = await req.formData();
+        const file = formdata.get('CSV_Requests');
+
+        if (!(file instanceof File)) {
+            return new Response("No CSV file provided.", { status: 400 });
+        }
+
+        const filePath = await saveUploadedFile(file);
+        await main(filePath);
+        return new Response("CSV processed successfully.", {
+            status: 200,
+            headers: {"Content-Type": "text/plain"}
+        });
+    }
+
+      return new Response("Not Found", { status: 404 });
+    },
+  });
+  
+  console.log(`Listening on http://localhost:${server.port}`);
+  
